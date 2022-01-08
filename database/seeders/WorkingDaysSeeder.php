8b85d9b568aae4a2cfd3b600e33198cea239ff6a
Megan <megan.ismaili@fshnstudent.info>
2022-01-08 01:22:46 +0100

    db

---
+++ database/seeders/WorkingDaysSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class WorkingDaysSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
